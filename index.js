0d639a0b38825506bfb7b542d82b161b23e28ee3
saifion33 <mxofficialplayer@gmail.com>
2023-05-24 14:18:58 +0530

    connect server to backend using mongoose

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express();
-app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+// cofigure environment variables
+dotenv.config()
+
+
+
+const PORT = process.env.PORT || 3000
+const DATABASE_URL = process.env.DATABASE_URL
+
+mongoose.connect(DATABASE_URL).then(() => app.listen(PORT, () => {
+    console.log(`Stackoverflow clone app listening on port ${PORT}`)
+})).catch((err) => console.log(err))
+
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
 app.get('/', (req, res) => {
     res.send('Stackoverflow server is Running...');
 })
