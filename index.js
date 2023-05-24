1b4735d7df184d0301aa5fea5617ec33056bfb84
saifion33 <mxofficialplayer@gmail.com>
2023-05-24 12:30:48 +0530

    first commit: initial server setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+
+const app = express();
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
+app.get('/', (req, res) => {
+    res.send('Stackoverflow server is Running...');
+})
