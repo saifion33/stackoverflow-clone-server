d16d6df099c4c60279c47c6672b3deeae9e6bf34
saifion33 <mxofficialplayer@gmail.com>
2023-05-25 10:52:52 +0530

    add delete endpoint to user routes

--- controllers/user.js
+++ controllers/user.js
@@ -28,5 +28,18 @@ export const updateUser = async (req, res) => {
 }
 
 export const deleteUser = async (req, res) => {
+    const { userId } = req.body;
+    const user = await User.findById(userId)
 
+    if (!user) {
+        return res.status(404).json({ status: 404, message: 'User does not exist', data: null })
+    }
+
+    if (user._id != userId) {
+        return res.status(401).json({ status: 401, message: 'User do not have permisson to delete account', data: null })
+    }
+
+    User.findByIdAndDelete(userId)
+        .then(user => res.status(200).json({ status: 200, message: 'User Account deleted successfully', data: user }))
+        .catch(err => res.status(500).json({ status: 500, message: 'There was an error', error: err }))
 }
