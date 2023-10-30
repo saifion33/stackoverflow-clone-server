bb7c26a2814a6a2ed92a05b06ca1bc0b54eb9241
saifion33 <mxofficialplayer@gmail.com>
2023-10-30 11:16:51 +0530

    fix some issue and set timeout for call token server request

--- controllers/calls.js
+++ controllers/calls.js
@@ -5,7 +5,7 @@ import shortid from 'shortid';
 
 export const makeCall = async (req, res) => {
     const userId = req.userId;
-    const { to,callType} = req.body;
+    const { to, callType } = req.body;
     const user = await User.findById(userId);
     if (!user || !to) {
         return res.status(404).json({ message: 'User not found' });
@@ -17,25 +17,33 @@ export const makeCall = async (req, res) => {
     callerStatusRef.set('waiting').catch(err => console.log(err));
     receiverStatusRef.get().then(async (snap) => {
         if (snap.val() === 'busy' || snap.val() === 'waiting') {
-            return res.status(409).json({ message: 'user is busy' });
+            callerStatusRef.set('idle').catch(err => console.log(err));
+            return res.status(409).json({ message: 'user is busy on another call.' });
         }
         receiverStatusRef.set('waiting').catch(err => console.log(err));
         callerStatusRef.set('waiting').catch(err => console.log(err));
-        const callId=shortid.generate();
-        const callToken=await (await axios.get(`https://agora-token-server-gafs.onrender.com/rtc/${callId}/publisher/uid/0/`)).data.rtcToken;
-        callRequestRef.set({callerId: user.fuid, callerName: user.displayName, callState: 'default',callId,callToken,callType});
+        const callId = shortid.generate();
+        const callToken = await (await axios.get(`https://agora-token-server-gafs.onrender.com/rtc/${callId}/publisher/uid/0/`,{timeout:1000*40})).data.rtcToken;
+        callRequestRef.set({ callerId: user.fuid, callerName: user.displayName, callState: 'default', callId, callToken, callType });
         setTimeout(async () => {
-            const onGoingCall = await (await onGoingCallRef.get()).val()
-            callRequestRef.set({}).catch(err=>console.log(err));
+            const onGoingCall = await onGoingCallRef.get().then(snap=>snap.exists());
+            callRequestRef.set({}).catch(err => console.log(err));
             if (!onGoingCall) {
                 receiverStatusRef.set('idle').catch(err => console.log(err));
                 callerStatusRef.set('idle').catch(err => console.log(err));
             }
         }, 1000 * 30)
-        res.status(200).json({ message: 'making call...',callData:{callId,callToken}});
+        res.status(200).json({ message: 'making call...', callData: { callId, callToken } });
     }).catch((err) => {
-        console.log(err)
-        res.status(500).json({ message: 'Internal Server error.'});
+        receiverStatusRef.set('idle').catch(err => console.log(err));
+        callerStatusRef.set('idle').catch(err => console.log(err));
+        callRequestRef.set({});
+        onGoingCallRef.set({});
+        console.log(err);
+        if (err.code === 'ECONNABORTED') {
+            return res.status(504).json({message:'Request Timeout plese try again.'});
+        }
+        res.status(500).json({ message: 'Internal Server error.' });
     });
 
 }
